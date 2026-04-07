2f62e95b2435400134e00efe02753d25d94da06b
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2026-04-06 21:53:16 -0300

    Aula 01 em desenvolvimento

---
+++ src/Tarefa.ts
@@ -0,0 +1,19 @@
+export class Tarefa {
+    private _descricao: string;
+    public concluida: boolean;
+    public readonly id: number;
+
+    constructor(id: number, descricao: string){
+        this.id = id;
+        this._descricao = descricao;
+        this.concluida = false;
+    }
+
+    public get descricao(): string {
+        return this._descricao;
+    }
+
+    public marcarComoConcluida(): void {
+        this.concluida = true;
+    }
+}
